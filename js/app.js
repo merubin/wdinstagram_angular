215202b2478bfa03c74c70d2f6a18b17d94ce99c
Michael Rubin <michael@rubinsworld.com>
2016-10-25 20:02:05 -0400

    Part 1 index working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,40 @@
-"use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+var winstagrams = [
+      {photo_url: "http://m9.i.pbase.com/u46/mrubin/medium/39911299.chinapeanut1.jpg",
+          author: "Mike Rubin",
+            body: "Nice Picture of a cat" },
+            {photo_url: "http://m7.i.pbase.com/u21/mrubin/large/36870637.hokie.jpg",
+                author: "Alex ",
+                  body: "Go Hokies" }]
+
+  angular
+  .module("winstagram", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .controller("WinstagramIndexController", [
+    WinstagramIndexControllerFunction
+  ]);
+
+function WinstagramIndexControllerFunction(){
+console.log('in the Winstragram Controller')
+  this.winstagrams=winstagrams
+  console.log(this.winstagrams[0].author)
+}
+
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("winstagramIndex",{
+    url:"/wg",
+    templateUrl: "js/ng-views/index.html",
+    controller:"WinstagramIndexController",
+    controllerAs: "vm"
+  })
+  .state("winstagramShow", {
+    url:"/wg/:id",
+    templateUrl: "/js/ng-views/show.html"
+  });
+}
